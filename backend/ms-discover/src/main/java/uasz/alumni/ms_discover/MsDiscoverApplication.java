a30c0f48787f21f9bac5795e11af650c85b25642
eadarak00 <eadarak2001@gmail.com>
2025-11-14 11:01:49 +0000

    feat: initialisation du microservice discover

---
+++ backend/ms-discover/src/main/java/uasz/alumni/ms_discover/MsDiscoverApplication.java
@@ -0,0 +1,13 @@
+package uasz.alumni.ms_discover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDiscoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDiscoverApplication.class, args);
+	}
+
+}
