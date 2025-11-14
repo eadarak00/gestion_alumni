697bb68131513bd202e14932040109798893261e
eadarak00 <eadarak2001@gmail.com>
2025-11-14 22:15:24 +0000

    feat: ajout de l'annotation de enableEurekaServer pour indiquer que le ms-discover est un serveur eureka

--- backend/ms-discover/src/main/java/uasz/alumni/ms_discover/MsDiscoverApplication.java
+++ backend/ms-discover/src/main/java/uasz/alumni/ms_discover/MsDiscoverApplication.java
@@ -2,12 +2,14 @@ package uasz.alumni.ms_discover;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MsDiscoverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsDiscoverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MsDiscoverApplication.class, args);
+    }
 
 }
