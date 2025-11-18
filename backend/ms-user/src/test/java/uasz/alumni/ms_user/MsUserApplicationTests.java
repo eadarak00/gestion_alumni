670f7bcd900ac63a1656fd85fa04d93def4b2090
Ansou <ansouknt@gmail.com>
2025-11-18 22:17:56 +0000

    feat(ms-user): ajout test Argon2, ajustements config tests, ajout entity Utilisateur & migration

--- backend/ms-user/src/test/java/uasz/alumni/ms_user/MsUserApplicationTests.java
+++ backend/ms-user/src/test/java/uasz/alumni/ms_user/MsUserApplicationTests.java
@@ -2,11 +2,12 @@ package uasz.alumni.ms_user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 
-@SpringBootTest(properties = {"spring.config.import=", "spring.cloud.config.enabled=false"},
-	excludeAutoConfiguration = {DataSourceAutoConfiguration.class, FlywayAutoConfiguration.class})
+@SpringBootTest(properties = {"spring.config.import=", "spring.cloud.config.enabled=false"})
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, FlywayAutoConfiguration.class})
 class MsUserApplicationTests {
 
 	@Test
