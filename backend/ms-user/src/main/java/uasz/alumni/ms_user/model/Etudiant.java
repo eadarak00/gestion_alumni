89dc496188b8e54269cc6f6196882a19e6a40580
eadarak00 <eadarak2001@gmail.com>
2025-11-19 15:41:51 +0000

    refactor(etudiant): amelioration de la validation des donnees

--- backend/ms-user/src/main/java/uasz/alumni/ms_user/model/Etudiant.java
+++ backend/ms-user/src/main/java/uasz/alumni/ms_user/model/Etudiant.java
@@ -1,22 +1,32 @@
 package uasz.alumni.ms_user.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import jakarta.persistence.Entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+
+@Entity
+@DiscriminatorValue("ETUDIANT")
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Data               
-@Entity
-@DiscriminatorValue("ETUDIANT") // valeur pour la colonne type_utilisateur
+@ToString(callSuper = true)
 public class Etudiant extends Utilisateur {
 
     @Column(unique = true, nullable = false)
+    @NotBlank(message = "Le numéro de carte étudiant est obligatoire")
     private String numeroCarteEtudiant;
-    
+
+    @NotBlank(message = "Le niveau est obligatoire")
     private String niveau;
+
+    @NotBlank(message = "La filière est obligatoire")
     private String filiere;
-}       
+}
