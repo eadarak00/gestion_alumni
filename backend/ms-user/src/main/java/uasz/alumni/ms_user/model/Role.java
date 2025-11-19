8364dc528de3299c6952a14c21ffe729328c5546
eadarak00 <eadarak2001@gmail.com>
2025-11-19 15:53:49 +0000

    feat(Role): extension de la classe de base

--- backend/ms-user/src/main/java/uasz/alumni/ms_user/model/Role.java
+++ backend/ms-user/src/main/java/uasz/alumni/ms_user/model/Role.java
@@ -1,22 +1,24 @@
 package uasz.alumni.ms_user.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;  
-import jakarta.persistence.GenerationType;
+import lombok.ToString;
+import uasz.alumni.ms_user.common.entity.BaseEntity;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-public class Role {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) 
-    private Long id;
+@ToString(callSuper = true)
+public class Role extends BaseEntity {
+
+    @NotBlank(message = "Le libellé du rôle est obligatoire")
+    @Column(nullable = false, unique = true, length = 50)
     private String libelle;
 }
