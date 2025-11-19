bf62f07262eed6d6c20200188588e849694abf06
eadarak00 <eadarak2001@gmail.com>
2025-11-19 16:44:26 +0000

    feat(Documentation) : Swagger pour la classe role

--- backend/ms-user/src/main/java/uasz/alumni/ms_user/model/Role.java
+++ backend/ms-user/src/main/java/uasz/alumni/ms_user/model/Role.java
@@ -1,8 +1,10 @@
 package uasz.alumni.ms_user.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,9 +18,19 @@ import uasz.alumni.ms_user.common.entity.BaseEntity;
 @Getter
 @Setter
 @ToString(callSuper = true)
+@Schema(description = "Entité représentant un rôle dans le système UASZ Alumni")
 public class Role extends BaseEntity {
 
     @NotBlank(message = "Le libellé du rôle est obligatoire")
+    @Size(min = 2, max = 50, message = "Le libellé doit contenir entre 2 et 50 caractères")
     @Column(nullable = false, unique = true, length = 50)
+    @Schema(
+        description = "Libellé unique du rôle",
+        example = "ADMINISTRATEUR",
+        requiredMode = Schema.RequiredMode.REQUIRED,
+        minLength = 2,
+        maxLength = 50,
+        pattern = "^[A-Z_]+$"
+    )
     private String libelle;
 }
