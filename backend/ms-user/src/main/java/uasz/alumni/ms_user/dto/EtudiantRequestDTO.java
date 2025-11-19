fa8ac97128c49a6b3a7e2357282f0ed0dd5ea25d
eadarak00 <eadarak2001@gmail.com>
2025-11-19 17:14:57 +0000

    refactor(Etudiant): Changement du DTO Inscription en Request

---
+++ backend/ms-user/src/main/java/uasz/alumni/ms_user/dto/EtudiantRequestDTO.java
@@ -0,0 +1,38 @@
+package uasz.alumni.ms_user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EtudiantRequestDTO {
+
+    @NotBlank(message = "Le nom est obligatoire")
+    private String nom;
+
+    @NotBlank(message = "Le prénom est obligatoire")
+    private String prenom;
+
+    @Email(message = "Email invalide")
+    @NotBlank(message = "L'email est obligatoire")
+    private String email;
+
+    @NotBlank(message = "Le username est obligatoire")
+    private String username;
+
+    @NotBlank(message = "Le mot de passe est obligatoire")
+    private String motDePasse;
+
+    private String telephone;
+
+    @NotBlank(message = "Le numéro de carte est obligatoire")
+    private String numeroCarteEtudiant;
+
+    @NotBlank(message = "Le niveau est obligatoire")
+    private String niveau;
+
+    @NotBlank(message = "La filiére est obligatoire")
+    private String filiere;
+}
