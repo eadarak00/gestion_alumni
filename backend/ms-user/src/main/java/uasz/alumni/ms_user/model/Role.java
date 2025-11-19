50a0871b952e23fca373dd4d48606255c825d5ef
eadarak00 <eadarak2001@gmail.com>
2025-11-19 15:36:58 +0000

    refactor(Role): utilisation des getters et setters

--- backend/ms-user/src/main/java/uasz/alumni/ms_user/model/Role.java
+++ backend/ms-user/src/main/java/uasz/alumni/ms_user/model/Role.java
@@ -1,20 +1,19 @@
 package uasz.alumni.ms_user.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;  
 import jakarta.persistence.GenerationType;
 
 @Entity
-@Table(name = "roles")
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
-
+@Getter
+@Setter
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) 
