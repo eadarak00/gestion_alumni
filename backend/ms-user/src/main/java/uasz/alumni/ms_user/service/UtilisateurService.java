1827441244c55f45c17cbc0eb1c2895a93011eba
eadarak00 <eadarak2001@gmail.com>
2025-11-20 21:38:59 +0000

    feat(Utilisateur): Creation de la methode pour recuperer un utilisateur de par son email

--- backend/ms-user/src/main/java/uasz/alumni/ms_user/service/UtilisateurService.java
+++ backend/ms-user/src/main/java/uasz/alumni/ms_user/service/UtilisateurService.java
@@ -1,11 +1,15 @@
 package uasz.alumni.ms_user.service;
 
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jakarta.persistence.EntityNotFoundException;
+import uasz.alumni.ms_user.common.exception.ResourceNotFoundException;
 import uasz.alumni.ms_user.dto.UtilisateurResponseDTO;
 import uasz.alumni.ms_user.mapper.UtilisateurMapper;
+import uasz.alumni.ms_user.model.Utilisateur;
 import uasz.alumni.ms_user.repository.UtilisateurRepository;
 
 import java.util.List;
@@ -33,4 +37,10 @@ public class UtilisateurService {
                 .map(utilisateurMapper::toDto)
                 .toList();
     }
+
+    public Utilisateur getUtilisateurByEmail(@NonNull String email) {
+        return utilisateurRepository.findByEmail(email)
+                .orElseThrow(() -> new ResourceNotFoundException("Utilisateur non trouvé avec email : " + email));
+    }
+
 }
