97cecac6d5ee907d1f84ac39fe2002b46142a3ec
eadarak00 <eadarak2001@gmail.com>
2025-11-20 21:43:54 +0000

    feat: centralisation des endpoints d'authentification

--- backend/ms-user/src/main/java/uasz/alumni/ms_user/controller/AuthentificationController.java
+++ backend/ms-user/src/main/java/uasz/alumni/ms_user/controller/AuthentificationController.java
@@ -1,28 +1,27 @@
 package uasz.alumni.ms_user.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import uasz.alumni.ms_user.dto.EtudiantRequestDTO;
 import uasz.alumni.ms_user.dto.EtudiantResponseDTO;
 import uasz.alumni.ms_user.service.EtudiantService;
 
 @RestController
-@RequestMapping("/api/v1/etudiants")
+@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
-@Tag(name = "Étudiant", description = "Endpoints pour les étudiants")
-public class EtudiantController {
-
-    private final EtudiantService etudiantService;
+public class AuthentificationController {
+     private final EtudiantService etudiantService;
 
     @Operation(summary = "Inscrire un nouvel étudiant", description = "Crée un nouveau compte étudiant.")
     @ApiResponse(responseCode = "201", description = "Étudiant inscrit avec succès", content = @Content(mediaType = "application/json", schema = @Schema(implementation = EtudiantResponseDTO.class)))
