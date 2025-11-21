4678e6b326db1146e51d94529d0c2935bd72e71e
eadarak00 <eadarak2001@gmail.com>
2025-11-21 00:13:42 +0000

    feat: creation de la classe alumni

---
+++ backend/ms-user/src/main/java/uasz/alumni/ms_user/model/Alumni.java
@@ -0,0 +1,26 @@
+package uasz.alumni.ms_user.model;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@DiscriminatorValue("ALUMNI")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true)
+@Schema(description = "Entité représentant un alumni de l'UASZ")
+public class Alumni extends Utilisateur {
+    private String profession;
+    private String entreprise;
+}
