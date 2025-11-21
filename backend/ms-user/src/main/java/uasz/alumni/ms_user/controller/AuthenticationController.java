23988169bddeef3a5ed59b28b6aec298141e171b
eadarak00 <eadarak2001@gmail.com>
2025-11-21 00:30:30 +0000

    feat: inscription alumni

--- backend/ms-user/src/main/java/uasz/alumni/ms_user/controller/AuthenticationController.java
+++ backend/ms-user/src/main/java/uasz/alumni/ms_user/controller/AuthenticationController.java
@@ -12,17 +12,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import uasz.alumni.ms_user.dto.AlumniRequestDTO;
+import uasz.alumni.ms_user.dto.AlumniResponseDTO;
 import uasz.alumni.ms_user.dto.EtudiantRequestDTO;
 import uasz.alumni.ms_user.dto.EtudiantResponseDTO;
+import uasz.alumni.ms_user.service.AlumniService;
 import uasz.alumni.ms_user.service.EtudiantService;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
-@Tag(name = "Inscription Étudiant", description = "Endpoints pour l'inscription des étudiants")
+@Tag(name = "Inscription", description = "Endpoints pour l'inscription des étudiants")
 public class AuthenticationController {
 
     private final EtudiantService etudiantService;
+    private final AlumniService alumniService;
 
     @Operation(summary = "Inscrire un nouvel étudiant", description = "Crée un nouveau compte étudiant.")
     @ApiResponse(responseCode = "201", description = "Étudiant inscrit avec succès", content = @Content(mediaType = "application/json", schema = @Schema(implementation = EtudiantResponseDTO.class)))
@@ -34,5 +39,14 @@ public class AuthenticationController {
                 .body(etudiantService.inscrireEtudiant(dto));
     }
 
+    @Operation(summary = "Inscrire un nouvel alumni", description = "Crée un nouveau compte alumni.")
+    @ApiResponse(responseCode = "201", description = "Alumni inscrit avec succès", content = @Content(mediaType = "application/json", schema = @Schema(implementation = EtudiantResponseDTO.class)))
+    @PostMapping("/inscription-alumni")
+    public ResponseEntity<AlumniResponseDTO> inscrireAlumni(
+            @Valid @RequestBody AlumniRequestDTO dto) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(alumniService.inscrireAlumni(dto));
+    }
 
 }
