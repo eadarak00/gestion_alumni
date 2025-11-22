df37f51afeaf46a24b71944507c32deb2859aa97
eadarak00 <eadarak2001@gmail.com>
2025-11-22 09:40:19 +0000

    reset

--- backend/ms-user/src/main/java/uasz/alumni/ms_user/common/exception/ResourceAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package uasz.alumni.ms_user.common.exception;
-
-public class ResourceAlreadyExistsException extends RuntimeException {
-
-    public ResourceAlreadyExistsException(String message) {
-        super(message);
-    }
-}
